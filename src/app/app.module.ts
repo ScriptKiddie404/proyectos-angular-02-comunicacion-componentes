4a12f8afb90eedbd6e6c3f62aa862158fc307e29
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-12-09 21:03:37 -0700

    Componentes padre e hijo creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponenteHijoComponent } from './componente-hijo/componente-hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteHijoComponent
   ],
   imports: [
     BrowserModule
