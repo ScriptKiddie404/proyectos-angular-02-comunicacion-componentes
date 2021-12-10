ad1b18c38807929d2b31c71eb2061387da174e9b
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-12-09 21:04:04 -0700

    COmponentes creados

---
+++ src/app/componente-hijo/componente-hijo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'componente-hijo',
+  templateUrl: './componente-hijo.component.html',
+  styleUrls: ['./componente-hijo.component.css'],
+})
+export class ComponenteHijoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
